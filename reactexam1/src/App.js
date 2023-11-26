70847a7175acd3a95819b27e0a862e8e8e2fcdca
EJ <hanej93@naver.com>
2023-11-26 20:38:37 +0900

    reactexam1

---
+++ reactexam1/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MyHeader from "./MyHeader";
+import Counter from "./Counter";
+
+function App() {
+  const number = 5;
+
+  return (
+    <div>
+      <MyHeader />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
