7106102ca9f973309e22983df0abcc8e4f2b4c73
EJ <hanej93@naver.com>
2023-12-10 23:21:49 +0900

    (서브 챕터) 흔히 발생하는 버그 수정 하기

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -77,7 +77,7 @@ const dummyData = [
 function App() {
   const [data, dispatch] = useReducer(reducer, dummyData);
 
-  const dataId = useRef(0);
+  const dataId = useRef(7);
   // CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({
